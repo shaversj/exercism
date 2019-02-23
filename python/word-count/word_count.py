19ab80a38a8c307859be16c8abb8b7a7fb6ae7df
shaversj <shaversj@gmail.com>
2019-02-22 23:55:09 -0500

    word_count updates

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,24 +1,16 @@
 from collections import Counter
-from string import punctuation
+import re
 
 
 def word_count(phrase):
-    table = str.maketrans(dict.fromkeys(punctuation))
-    phrase_without_punctuation = phrase.translate(table)
 
-    phrase_without_punctuation = phrase_without_punctuation.lower()
+    phrase = phrase.lower()
+    word = re.compile(r"(?<=')\w+(?=')|[a-z][^\t,._: !#$%&()*+-]+|\d")
+    count_words = Counter(word.findall(phrase)).most_common()
 
-    phrase_no_whitespace_or_punc = phrase_without_punctuation.strip().split()
-
-    count_words = Counter(phrase_no_whitespace_or_punc).most_common()
     final_dict = {}
 
     for k, v in count_words:
         final_dict[k] = v
 
     return final_dict
-
-
-print(word_count("hey,my_spacebar_is_broken."))
-# print(word_count("Joe can't tell between 'large' and large."))
-# print(word_count("one,\ntwo,\nthree"))
