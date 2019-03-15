bbb5ed076913b35e5d5b05619cc33640daf3374a
shaversj <shaversj@gmail.com>
2019-03-15 10:23:56 -0400

    test_lender_owes_borrower_less_than_new_loan

--- python/rest-api/rest_api.py
+++ python/rest-api/rest_api.py
@@ -36,10 +36,46 @@ class RestAPI(object):
             borrower = payload["borrower"]
             amount = payload["amount"]
 
-            self.apply_iou_to_database(lender, borrower, amount)
+            # self.apply_iou_to_database(lender, borrower, amount)
+            self.apply_iou(lender, borrower, amount)
             response = self.build_iou_response(lender, borrower)
 
             return json.dumps(response)
+
+    def apply_iou(self, lender, borrower, amount):
+
+        for value in self.database.get("users"):
+            if value["name"] == lender:
+                if borrower in value["owes"].keys():
+                    value["owes"][borrower] -= amount
+                    value["balance"] += amount
+                    if value["owes"][borrower] == 0:
+                        del value["owes"][borrower]
+                    if value["owes"][borrower] < 0:
+                        owed_balance = abs(value["owes"][borrower])
+                        value["owed_by"] = {borrower: owed_balance}
+                        del value["owes"][borrower]
+                else:
+                    value["owed_by"] = {borrower: amount}
+                    value["balance"] += int(amount)
+
+        for value in self.database.get("users"):
+            if value["name"] == borrower:
+                if lender in value["owed_by"]:
+                    value["owed_by"][lender] -= amount
+                    value["balance"] -= amount
+                    if value["owed_by"][lender] == 0:
+                        del value["owed_by"][lender]
+                    if value["owed_by"][lender] < 0:
+                        new_amount = abs(value["owed_by"][lender])
+                        new_entry = {lender: new_amount}
+                        value["owes"].update(new_entry)
+                        del value["owed_by"][lender]
+
+                else:
+                    new_entry = {lender: amount}
+                    value["owes"].update(new_entry)
+                    value["balance"] -= amount
 
     def apply_iou_to_database(self, lender, borrower, amount):
 
