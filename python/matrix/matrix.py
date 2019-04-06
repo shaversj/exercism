e2bd1e75a2e657a0a07cf5661eaf5ab745323c53
shaversj <shaversj@gmail.com>
2019-04-06 10:53:23 -0400

    matrix final

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,16 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        self.matrix_string = matrix_string
+        self.matrix_list = []
+        for numbers in self.matrix_string.split("\n"):
+            split_numbers = numbers.split()
+            self.matrix_list.append(split_numbers)
 
     def row(self, index):
-        pass
+        index = index - 1
+        return list(map(int, self.matrix_list[index]))
 
     def column(self, index):
-        pass
+        index = index - 1
+        column_entries = [row[index] for row in self.matrix_list]
+        return list(map(int, column_entries))
