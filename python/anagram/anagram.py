cd1d18a0f5f0a2e82c9b493e3da6cb4bb8d8135c
shaversj <shaversj@gmail.com>
2019-07-10 12:58:06 -0400

    version 2

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,7 +1,22 @@
-from itertools import permutations
-
-
 def find_anagrams(word, candidates):
+    sorted_word = "".join(sorted(word.lower()))
+    sorted_candidates = [
+        "".join(sorted(candidate))
+        for candidate in list(map(str.lower, candidates))
+        if candidate != word.lower()
+    ]
+
+    matches = []
+    for index, sorted_string in enumerate(sorted_candidates):
+        if sorted_string == sorted_word:
+            matches.append(candidates[index])
+
+    return matches
+
+
+def find_anagrams_old(word, candidates):
+    from itertools import permutations
+
     lowered_word = word.lower()
     lowered_candidates = list(map(str.lower, candidates))
     possible_values = permutations(lowered_word)
